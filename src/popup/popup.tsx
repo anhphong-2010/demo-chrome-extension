0fedc10f0dbb9ed33aa1d0fe113a0157fdd18bfc
anhphong.2010 <anhphongnw.2010@gmail.com>
2023-04-12 15:15:59 +0700

    ^ search bar

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -3,14 +3,35 @@ import { createRoot } from "react-dom/client";
 import "../assets/tailwind.css";
 
 const test = (
-  <div>
-    <h1 className="text-4xl bg-green-500">Hello world</h1>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius at, ab
-      dolorum iusto sequi enim adipisci debitis voluptates eum architecto eaque
-      reiciendis atque delectus similique perspiciatis, nam excepturi. Maiores,
-      ex.
-    </p>
+  <div className="p-4">
+    <h1 className="text-center text-2xl mb-4">Search keywords</h1>
+    <div className="max-w-md mx-auto">
+      <div className="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
+        <div className="grid place-items-center h-full w-12 text-gray-300">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </div>
+
+        <input
+          className="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
+          type="text"
+          id="search"
+          placeholder="Search something.."
+        />
+      </div>
+    </div>
   </div>
 );
 
